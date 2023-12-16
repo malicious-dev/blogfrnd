3b1d04687db0f7e7e0bae7f1c242fec68ad3c019
Malicious-dev <Programming1hars@gmail.com>
2023-12-16 18:03:43 +0530

    Add FlatList to display multiple FeedPosts

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import FeedPost from "./src/components/FeedPost";
+import posts from "./assets/data/posts";
 
+const post = {
+  id: "p1",
+  createdAt: "19 m",
+  User: {
+    id: "u1",
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
+    name: "Vadim Savin",
+  },
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+  numberOfLikes: 11,
+  numberOfShares: 2,
+};
 
 export default function App() {
   return (
     <View style={ styles.container }>
-
-      <FeedPost />
+      <FlatList
+        data={ posts }
+        renderItem={ ({ item }) => <FeedPost post={ item } /> }
+        keyExtractor={ (item) => item.id }
+        showsVerticalScrollIndicator={ false }
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,7 +36,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "lightgray",
     alignItems: "center",
     justifyContent: "center",
   }
